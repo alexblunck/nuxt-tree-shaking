ee46d983a2c98367f2da7b350da7bcfd5d231cf3
Alexander Blunck <alex@blunck.me>
2021-04-06 20:50:59 +0200

    Initial commit

---
+++ components/ComponentA.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>This is ComponentA</div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentA'
+}
+</script>
---
+++ components/ComponentB.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>This is ComponentB</div>
+</template>
+
+<script>
+export default {
+  name: 'ComponentB'
+}
+</script>
